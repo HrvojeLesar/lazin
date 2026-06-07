29db143c1843271dc6e4ea28024a0d0250f23b9e
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2026-06-07 21:46:23 +0200

    Simplify pipeline

--- crates/lazin_pipeline/src/lib.rs
+++ crates/lazin_pipeline/src/lib.rs
@@ -1,65 +1,67 @@
+use std::marker::PhantomData;
+
 pub struct Unvalidated;
 pub struct Passed;
-pub struct Failed<E>(pub E);
+pub struct FailedStep<E>(pub E);
 
 pub type PipelineResult<E> = Result<(), E>;
-pub type ValidatorFn<T, E> = fn(&T) -> PipelineResult<E>;
-
-type Pipeline<T, E> = Result<ValidationStep<T, Passed>, ValidationStep<T, Failed<E>>>;
+type Pipeline<T, E> = Result<ValidationStep<T, Passed>, FailedStep<E>>;
 
 pub trait Bind<T, E> {
-    fn bind(self, validator: ValidatorFn<T, E>) -> Pipeline<T, E>;
+    fn bind<F>(self, validator: F) -> Pipeline<T, E>
+    where
+        F: Fn(&T) -> PipelineResult<E>;
     fn result(self) -> PipelineResult<E>;
 }
 
 pub struct ValidationStep<T, State = Unvalidated> {
     data: T,
-    state: State,
+    state: PhantomData<State>,
 }
 
 impl<T> ValidationStep<T, Unvalidated> {
     pub fn new(data: T) -> Self {
         Self {
             data,
-            state: Unvalidated,
+            state: PhantomData,
         }
     }
 }
 
 impl<T, E> Bind<T, E> for Pipeline<T, E> {
-    fn bind(self, validator: ValidatorFn<T, E>) -> Pipeline<T, E> {
+    fn bind<F>(self, validator: F) -> Pipeline<T, E>
+    where
+        F: Fn(&T) -> PipelineResult<E>,
+    {
         let step = self?;
         match validator(&step.data) {
             Ok(_) => Ok(ValidationStep {
                 data: step.data,
-                state: Passed,
+                state: PhantomData,
             }),
-            Err(e) => Err(ValidationStep {
-                data: step.data,
-                state: Failed(e),
-            }),
+            Err(e) => Err(FailedStep(e)),
         }
     }
 
     fn result(self) -> PipelineResult<E> {
         match self {
             Ok(_) => Ok(()),
-            Err(step) => Err(step.state.0),
+            Err(step) => Err(step.0),
         }
     }
 }
 
 impl<T, E> Bind<T, E> for ValidationStep<T, Unvalidated> {
-    fn bind(self, validator: ValidatorFn<T, E>) -> Pipeline<T, E> {
+    fn bind<F>(self, validator: F) -> Pipeline<T, E>
+    where
+        F: Fn(&T) -> PipelineResult<E>,
+    {
         match validator(&self.data) {
             Ok(_) => Ok(ValidationStep {
                 data: self.data,
-                state: Passed,
+                state: PhantomData,
             }),
-            Err(e) => Err(ValidationStep {
-                data: self.data,
-                state: Failed(e),
-            }),
+            Err(e) => Err(FailedStep(e)),
         }
     }
 
