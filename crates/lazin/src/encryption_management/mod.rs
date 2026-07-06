2efcb4bd73c24067c64d606faa0b74ccf1e8c3ba
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2026-07-06 20:55:48 +0200

    Unfinished encryption management

--- crates/lazin/src/encryption_management/mod.rs
+++ crates/lazin/src/encryption_management/mod.rs
@@ -10,18 +10,38 @@ use std::path::Path;
 use lazin_error::{Context, LazinResult};
 use lazin_gpg_wrapper::DecryptOptions;
 
-use crate::cache::Cache;
+use crate::{
+    cache::{Cache, Entry, FileHash},
+    dotfiles::resolved_config::ResolvedConfig,
+};
+
 mod gitignore;
 
 const GPG_EXTENSION: &str = "gpg";
 
-pub struct EncryptionManager {
+pub struct EncryptionManager<'a> {
     cache: Cache,
+    resolved_config: &'a ResolvedConfig,
 }
 
-impl EncryptionManager {
-    pub fn new(cache: Cache) -> Self {
-        Self { cache }
+impl<'a> EncryptionManager<'a> {
+    pub fn new(cache: Cache, resolved_config: &'a ResolvedConfig) -> Self {
+        Self {
+            cache,
+            resolved_config,
+        }
+    }
+
+    fn do_something(&mut self) -> LazinResult {
+        let encrypted_values = self.resolved_config.encrypted_values();
+        for value in encrypted_values {
+            let old_entry = self.cache.add_entry(Entry::Encryption(
+                value.source.clone(),
+                FileHash::hash(&value.source)?,
+            ))?;
+        }
+
+        Ok(())
     }
 
     pub fn decrypt(&mut self, file: &Path) -> LazinResult<()> {
